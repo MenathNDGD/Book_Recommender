39eb902d9ca7fc4c70cfb747db4fac821b4ee714
Menath Nadungoda <nadungmenath1@gmail.com>
2024-07-02 22:57:58 +0530

    divided to sections

---
+++ main.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter import messagebox
+import requests
+from PIL import ImageTk, Image
+from io import BytesIO
+import urllib.parse
+
+root = Tk ()
+root.title ("Book Recommend System")
+root.geometry ("1250x700")
+root.config (bg = "#111119")
+
+##################################################################################################################
+
+#icon
+
+##################################################################################################################
+
+root.mainloop ()
